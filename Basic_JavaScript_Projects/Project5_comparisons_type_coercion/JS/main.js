b1144781a1d3f17df031834f13d9cbe44472d046
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2021-11-18 10:54:53 -0800

    project 5

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+document.write(typeof "red");       //check type 
+document.write("10" + 5);           //type coercion
+document.write(2<4 && 6>9);         //boolean AND
+document.write (!(6>9) || (1>4));   //not and boolean OR
+document.write(10 == 5 + 5);        //checking if two values are equal
+document.write(10 === "purple");    //comparing value and type of two variables
