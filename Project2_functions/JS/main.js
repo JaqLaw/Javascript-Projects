21eb49df99a05f823fe730ee1dcb098500a848e8
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2021-11-17 20:22:43 -0800

    second assignment

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {   //naming the function
+    var sentence = "I am learning"; //declaring a variable
+    sentence += " a lot from this book!"; //concatenating something to the variable
+    document.getElementById("Concatenate").innerHTML = sentence; //displaying the result
+}
