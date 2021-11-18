5d957573d2d94e6527ebc336c9146043bd826227
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2021-11-17 20:05:15 -0800

    js assignment

---
+++ Basic_JavaScript_Projects/Project1_Expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var J = "Jaq", K = "Kat", L = " heart "; //defines variables
+var J = J.fontcolor("purple"); //changes color of variable
+var K = K.fontcolor("green"); //changes color of variable
+var L = L.fontcolor("red"); //changes color of variable
+
+document.write(J + L + K); //prints variables concatenated
+
+window.alert(7+7); //alerts with an expression
